7af186acc9af7a053832567c0dab2fee11518331
Ahmed BenSuliman <b13letu74@gmail.com>
2021-02-22 14:30:40 +0200

    first commit

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,57 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcService;
+using GrpcService.Protos;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //var input = new HelloRequest 
+            //{
+            //    Name="Ahmed Tareck Ben Suliman"
+            //};
+
+            //var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            //var client = new Greeter.GreeterClient(channel);
+
+            //var reply = await client.SayHelloAsync(input);
+
+            //Console.WriteLine(reply.Message);
+
+            var chanel = GrpcChannel.ForAddress("https://localhost:5001");
+
+            var customerClint = new Customer.CustomerClient(chanel);
+
+            var clientRequested = new CustomerLookupModel { UserId = 2 };
+
+            var customer = await customerClint.GetCustomerInfoAsync(clientRequested);
+
+            Console.WriteLine($"{customer.FirstName} { customer.LastName}");
+           
+            
+            Console.WriteLine();
+            Console.WriteLine("New Customer List");
+            Console.WriteLine();
+
+            
+
+            using (var call=customerClint.GetNewCustomers(new NewCustomerRequest()))
+            {
+                while(await call.ResponseStream.MoveNext())
+                {
+                    var currentCustomer = call.ResponseStream.Current;
+
+                    Console.WriteLine($"{ currentCustomer.FirstName} { currentCustomer.LastName} " +
+                        $"{ currentCustomer.EmailAddress} { currentCustomer.Age} { currentCustomer.IsAlive}");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GrpcService/Services/CustomersService.cs
@@ -0,0 +1,84 @@
+﻿using Grpc.Core;
+using GrpcService.Protos;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcService.Services
+{
+    public class CustomersService : Customer.CustomerBase
+    {
+        private readonly ILogger<CustomersService> _logger;
+
+        public CustomersService(ILogger<CustomersService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+        {
+            //return base.GetCustomerInfo(request, context); 
+
+            CustomerModel output = new CustomerModel();
+
+            if(request.UserId==1)
+            {
+                output.FirstName = "Ahmed";
+                output.LastName = "Tareck";
+
+            }
+            else if (request.UserId == 2)
+            {
+                output.FirstName = "Tareck";
+                output.LastName = "Ben";
+            }
+            else
+            {
+                output.FirstName = "Ali";
+                output.LastName = "Ahmed";
+            }
+
+            return Task.FromResult(output);
+        }
+
+        public override async Task GetNewCustomers(NewCustomerRequest request, IServerStreamWriter<CustomerModel> responseStream, ServerCallContext context)
+        {
+            List<CustomerModel> customers = new List<CustomerModel>
+            {
+                new CustomerModel
+                {
+                    FirstName="Ahmed",
+                    LastName="tareck",
+                    EmailAddress="ahmedtareckb@gmail.com",
+                    Age=23,
+                    IsAlive=true
+                },
+                new CustomerModel
+                {
+                    FirstName="Fathi",
+                    LastName="tareck",
+                    EmailAddress="ahmedtareckb@gmail.com",
+                    Age=24,
+                    IsAlive=true
+                },
+                new CustomerModel
+                {
+                    FirstName="Abdo",
+                    LastName="tareck",
+                    EmailAddress="ahmedtareckb@gmail.com",
+                    Age=25,
+                    IsAlive=true
+                }
+            };
+
+            foreach (var cust in customers)
+            {
+                await Task.Delay(1000);
+                await responseStream.WriteAsync(cust);
+            }
+        }
+
+    }
+}
